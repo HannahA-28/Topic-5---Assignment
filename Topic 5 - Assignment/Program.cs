81a55822c0ccc588bd91edec9abe5cb0c79c411d
Hannah Anderson <339729345@GoTVDSB.ca>
2025-03-21 14:41:47 -0400

    Starting part three!

--- Topic 5 - Assignment/Program.cs
+++ Topic 5 - Assignment/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hannah Anderson");
+
+            //Console.WriteLine("Part one");
+            //int weight;
+            //string planet;
+            //Console.WriteLine("What is your current weight?");
+            //int.TryParse(Console.ReadLine(), out weight);
+            //Console.WriteLine("I have information for the following planets: " +
+            //    "1.Venus 2.Mars 3.Jupiter 4.Saturn 5.Uranus 6.Neptune " +
+            //    "Which planet are you visiting?");
+            //planet = Console.ReadLine();
+            //if (planet == "1")
+            //    Console.WriteLine("Your weight would be " + weight * 0.78 + "pounds on that planet.");
+            //else if (planet == "2")
+            //    Console.WriteLine("Your weight would be " + weight * 0.39 + "pounds on that planet.");
+            //else if (planet == "3")
+            //    Console.WriteLine("Your weight would be " + weight * 2.65 + "pounds on that planet.");
+            //else if (planet == "4")
+            //    Console.WriteLine("Your weight would be " + weight * 1.17 + "pounds on that planet.");
+            //else if (planet == "5")
+            //    Console.WriteLine("Your weight would be " + weight * 1.05 + "pounds on that planet.");
+            //else if (planet == "6")
+            //    Console.WriteLine("Your weight would be " + weight * 1.23 + "pounds on that planet.");
+
+            //Console.WriteLine("Part two");
+            //int number;
+            //Console.WriteLine("Guess the number from 1 to 10.");
+            //int.TryParse(Console.ReadLine(), out number);
+            //if (number == 4)
+            //    Console.WriteLine("That is correct!");
+            //else
+            //    Console.WriteLine("Wrong, the number was 4!");
+
+            Console.WriteLine("Part three");
+
+            Console.WriteLine("Quiz time!");
+            Console.WriteLine("Question one: How many..."
+
         }
     }
 }
